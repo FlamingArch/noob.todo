0a0b694c307d172d1174523d8e6c17bfb336816f
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-08-12 07:42:19 +0530

    Initial Commit

---
+++ noob.todo/noob_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  noob_todoApp.swift
+//  noob.todo
+//
+//  Created by Harsh Chaturvedi on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct noob_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
